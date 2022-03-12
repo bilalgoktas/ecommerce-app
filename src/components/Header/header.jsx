52dd47ebfadf57d551c20549508d532df1b84937
Bilal Goktas <bgoktas97@gmail.com>
2022-03-13 00:21:26 +0100

    finished header styling

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,6 +2,8 @@ import classNames from "classnames";
 import React, { Component } from "react";
 import styles from "./header.module.css";
 import Cart from "../../assets/svg/cart.svg";
+import Logo from "../../assets/svg/logo.svg";
+import Arrow from "../../assets/svg/down-arrow.svg";
 
 class Header extends Component {
   render() {
@@ -15,10 +17,15 @@ class Header extends Component {
           <div className={styles.filterItem}>KIDS</div>
         </div>
         <div className={styles.centerContainer}>
-          <a href="#">icon</a>
+          <a href="#">
+            <img src={Logo} alt="logo" />
+          </a>
         </div>
         <div className={styles.rightContainer}>
-          <div className={styles.currencySwitcher}>usd</div>
+          <div className={styles.currencySwitcher}>
+            <span>$</span>
+            <img src={Arrow} />
+          </div>
           <div className={styles.miniCart}>
             <img src={Cart} alt="cart" />
           </div>
