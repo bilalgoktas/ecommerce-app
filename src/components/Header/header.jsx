968c72e4b38146b54fa06ae019e6666a195a3857
Bilal Goktas <bgoktas97@gmail.com>
2022-03-12 15:34:08 +0100

    initial commit

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import React, { Component } from "react";
+import styles from "./header.module.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className={styles.container}>
+        <div className={styles.leftContainer}>
+          <div className={classNames(styles.filterItem, styles.active)}>
+            WOMEN
+          </div>
+          <div className={styles.filterItem}>MEN</div>
+          <div className={styles.filterItem}>KIDS</div>
+        </div>
+        <div className={styles.centerContainer}>
+          <a href="#">icon</a>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.currencySwitcher}>usd</div>
+          <div className={styles.miniCart}>mini cart</div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
